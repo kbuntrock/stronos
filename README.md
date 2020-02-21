d9b641c04e0b9ecc68d4f94001d43b2668a3b08c
Kevin Buntrock <kevin.buntrock@kleegroup.com>
2020-02-21 11:28:54 +0100

    Correction nb seconds

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This project use as a dependency java-lame : https://github.com/nwaldispuehl/java-lame
+Stream your vinyle through a Sonos device
 
-Article de medium intéressant (matos rasberry) : https://medium.com/@dupontgu/how-to-stream-your-record-player-throughout-your-home-for-cheap-fb044368a240
+This project use as a dependency java-lame : https://github.com/nwaldispuehl/java-lame
--- front/app/src/app/info/info.component.ts
+++ front/app/src/app/info/info.component.ts
@@ -31,9 +31,9 @@ export class InfoComponent implements OnInit {
   }
 
   public formatSeconds(seconds: number): string {
-    console.info('nb seconds : ' + seconds);
+    console.info('In seconds : ' + seconds);
     const d = Math.floor(seconds / 3600 / 24);
-    const h = Math.floor(seconds / 3600);
+    const h = Math.floor((seconds - (d * 3600 * 24))/ 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor((seconds % 3600) % 60);
 
